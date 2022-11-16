2515ee080132605181f7caddc92228f19020cce7
Oleksandr Fomin <qilibriumultra@gmail.com>
2022-11-17 00:54:48 +0200

    update input functional

--- src/main/java/HttpImageStatusCli.java
+++ src/main/java/HttpImageStatusCli.java
@@ -4,18 +4,22 @@ public class HttpImageStatusCli {
     void askStatus() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter HTTP status code");
+        System.out.println("Enter HTTP status code or exit");
 
-        while (scanner.hasNext()) {
-            if (scanner.hasNextInt()) {
-                new HttpStatusImageDownloader().downloadStatusImage(scanner.nextInt());
-                System.out.println("Enter HTTP status code or exit");
-            } else if (scanner.hasNext("exit")) {
-                scanner.close();
-            } else {
-                System.out.println("Please enter valid number\n");
-                new HttpImageStatusCli().askStatus();
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
+                    new HttpStatusImageDownloader().downloadStatusImage(scanner.nextInt());
+                    System.out.println("Enter HTTP status code or exit");
+                } else if (scanner.hasNext("exit")) {
+                    System.exit(0);
+                    scanner.close();
+                } else {
+                    System.out.println("Please enter valid number\n");
+                    new HttpImageStatusCli().askStatus();
+                }
+
             }
+
         }
-    }
+
 }
